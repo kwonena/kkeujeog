e03d85d89576228d53286b8ed56a7078b6a2f8c4
Kwon Ena <eeena98@naver.com>
2022-04-07 23:50:55 +0900

    Add : 소셜로그인 관련 auth api 생성

---
+++ src/api/auth.js
@@ -0,0 +1,35 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
+import * as firebase from "./firebase.js";
+
+// getAuth : firebase에서 간단한 로그인을 도와주는 함수
+const auth = getAuth(firebase.firebaseApp);
+
+// 소셜별로 객체 생성
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+// 로그인 함수
+// 로그인은 소셜 정보가 담긴 providerName을 인자로 받음
+export const login = (providerName) => {
+  const authProvider = getProvider(providerName);
+
+  // signInWithPopup : 팝업창을 사용해 로그인 할 수 있게끔 호출
+  return signInWithPopup(auth, authProvider);
+};
+
+// 소셜로그인 종류에 따라
+export const getProvider = (providerName) => {
+  switch (providerName) {
+    case "Google":
+      return googleProvider;
+    case "Github":
+      return githubProvider;
+    default:
+      throw new Error(`not supported provider: ${providerName}`);
+  }
+};
