3ae5ae47bd5d3eec856282789e234cf88a7887b3
Kwon Ena <eeena98@naver.com>
2022-04-09 23:56:43 +0900

    feat : 소셜 로그인 구현

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import "./login.scss";
+import * as auth from "../../api/auth.js";
 
 const Login = () => {
+  // 로그인 함수
+  const onLogin = (event) => {
+    console.log(event.currentTarget.textContent);
+    auth // 클릭한 버튼의 텍스트를 인자로 login api에 전달
+      .login(event.currentTarget.textContent)
+      .then(console.log);
+  };
   return (
     <section className="login">
       <Header />
@@ -11,10 +19,14 @@ const Login = () => {
         <h1>Login</h1>
         <ul className="list">
           <li className="item">
-            <button className="button">Google</button>
+            <button className="button" onClick={onLogin}>
+              Google
+            </button>
           </li>
           <li className="item">
-            <button className="button">Github</button>
+            <button className="button" onClick={onLogin}>
+              Github
+            </button>
           </li>
         </ul>
       </section>
