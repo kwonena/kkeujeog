eeee339b02f636c3f7dbe02183961d930de52463
Kwon Ena <eeena98@naver.com>
2022-05-10 23:49:06 +0900

    Feat : header 로고 클릭시 메인 페이지 이동

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./header.scss";
 
 const Header = ({ onLogout }) => {
+  const navigate = useNavigate();
+
   return (
     <header className="header">
-      {!onLogout && <img className="logo" src="/images/logo.png" alt="logo" />}
-      <h1 className="title">끄적끄적</h1>
+      {!onLogout && (
+        <img
+          className="logo"
+          src="/images/logo.png"
+          alt="logo"
+          onClick={() => navigate("/main")}
+        />
+      )}
+      <h1 className="title" onClick={() => navigate("/main")}>
+        끄적끄적
+      </h1>
       {onLogout && <span className="comment">나만의 기록 사이트</span>}
       {onLogout && (
         <button className="logout" onClick={onLogout}>
